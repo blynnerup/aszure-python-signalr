c2d2d3f44e0488bcce207dcc84fa347473e2d06e
Brian Lynnerup Pedersen <brian.lynnerup.pedersen@gmail.com>
2024-06-06 03:49:32 +0200

    Create my own broadcast function

--- function_app.py
+++ function_app.py
@@ -7,7 +7,7 @@ app = func.FunctionApp()
 
 # Used by client to know where to call the function
 @app.route(route="negotiate", auth_level=func.AuthLevel.ANONYMOUS, methods=["POST"])
-@app.generic_input_binding(arg_name="connectionInfo", type="signalRConnectionInfo", hubName="bdotstsignal", connectionStringSetting="AzureSignalRConnectionString", direction="in")
+@app.generic_input_binding(arg_name="connectionInfo", type="signalRConnectionInfo", hubName="bdotstsignal", connectionStringSetting="AzureSignalRConnectionString")
 def negotiate(req: func.HttpRequest, connectionInfo) -> func.HttpResponse:
     logging.info(f"connectionInfo: {connectionInfo}")
     return func.HttpResponse(connectionInfo)
@@ -18,13 +18,52 @@ def index(req: func.HttpRequest) -> func.HttpResponse:
     f = open(os.path.dirname(os.path.realpath(__file__)) + '/content/index.html')
     return func.HttpResponse(f.read(), mimetype='text/html')
 
-# Triggers when a request is sent to the endpoint, prints 'arguments'
-@app.generic_trigger(arg_name="myTrigger", type="httpTrigger")
-@app.generic_output_binding(arg_name="signalRMessages", type="signalR", hubName="bdotstsignal", connectionStringSetting="AzureSignalRConnectionString")
-def broadcast(myTrigger: func.HttpRequest, signalRMessages: func.Out[str]) -> None:
-    logging.info("Broadcast started")
 
-    signalRMessages.set(json.dumps({
-        'target': 'newMessage',
-        'arguments': ['This is sent back']
-    }))
+@app.function_name(name="modify_message")
+@app.route(route="modify_message", methods=["POST"])
+@app.generic_output_binding(
+    arg_name="signalRMessages",
+    type="signalR",
+    hub_name="bdotstsignal",
+    connection_string_setting="AzureSignalRConnectionString"
+)
+def modify_message(req: func.HttpRequest, signalRMessages: func.Out[str]) -> func.HttpResponse:
+    logging.info('modify_message function received a request.')
+
+    try:
+        # Parse the JSON body
+        req_body = req.get_json()
+        message = req_body.get('message')
+
+        if message is None:
+            return func.HttpResponse(
+                "Please pass a message in the request body",
+                status_code=400
+            )
+
+        # Reverse the message
+        reversed_message = message[::-1]
+
+        # Create a SignalR message
+        signalr_message = {
+            'target': 'newMessage',
+            'arguments': [reversed_message]
+        }
+
+        # Set the SignalR message output
+        signalRMessages.set(json.dumps(signalr_message))
+
+        return func.HttpResponse(f"Message received and broadcasted: {reversed_message}")
+
+    except ValueError:
+        return func.HttpResponse(
+            "Invalid JSON body",
+            status_code=400
+        )
+
+    except Exception as e:
+        logging.error(f"Error processing the request: {str(e)}")
+        return func.HttpResponse(
+            "An error occurred processing the request",
+            status_code=500
+        )
