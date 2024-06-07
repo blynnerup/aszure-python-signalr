34e742d8e3df3be8f0bcbb6b16a729a28ced7544
Brian Lynnerup Pedersen <brian.lynnerup.pedersen@gmail.com>
2024-06-07 21:30:14 +0200

    Updates to function
    Created a status sender function. Created a mock query function which will wait in intervals to allow for mocking timeouts.

--- function_app.py
+++ function_app.py
@@ -1,69 +1,69 @@
 import azure.functions as func
-import json
-import logging
 import os
+import json 
+import logging
+import time
+import requests
 
 app = func.FunctionApp()
 
-# Used by client to know where to call the function
+etag = ''
+start_count = 0
+counter = 1
+
+
+@app.route(route="index", auth_level=func.AuthLevel.ANONYMOUS)
+def index(req: func.HttpRequest) -> func.HttpResponse:
+    f = open(os.path.dirname(os.path.realpath(__file__)) + '/content/index.html')
+    return func.HttpResponse(f.read(), mimetype='text/html')
+
 @app.route(route="negotiate", auth_level=func.AuthLevel.ANONYMOUS, methods=["POST"])
 @app.generic_input_binding(arg_name="connectionInfo", type="signalRConnectionInfo", hubName="bdotstsignal", connectionStringSetting="AzureSignalRConnectionString")
 def negotiate(req: func.HttpRequest, connectionInfo) -> func.HttpResponse:
     logging.info(f"connectionInfo: {connectionInfo}")
     return func.HttpResponse(connectionInfo)
 
-# Hosts the web page
-@app.route(route="index", auth_level=func.AuthLevel.ANONYMOUS)
-def index(req: func.HttpRequest) -> func.HttpResponse:
-    f = open(os.path.dirname(os.path.realpath(__file__)) + '/content/index.html')
-    return func.HttpResponse(f.read(), mimetype='text/html')
+@app.function_name(name="send_status")
+@app.route(route="send_status")
+@app.generic_output_binding(arg_name="signalRMessages", type="signalR", hubName="bdotstsignal", connectionStringSetting="AzureSignalRConnectionString")
+def send_status(req: func.HttpRequest, signalRMessages: func.Out[str]) -> str:
+    """
+    Sends status to SignalR
+    """
+    req_body = req.get_json()
+    status = req_body.get('target')
+    signalRMessages.set(json.dumps({
+        'target': 'newMessage',
+        'arguments': ['this is a status message']
+    }))
+    return "ok"
 
+@app.function_name(name="Mockquery")
+@app.route(route="req")
+@app.generic_output_binding(arg_name="signalRMessages", type="signalR", hubName="bdotstsignal", connectionStringSetting="AzureSignalRConnectionString")
+def mock_query(req: func.HttpRequest, signalRMessages: func.Out[str]) -> str:
+    # req_body = req.get_json()
+    # user = req_body.get('user')
+    # user = req.params.get("user")
+    req_body = req.get_json()
+    waitTime1 = req_body.get('wait1')
+    waitTime2 = req_body.get('wait2')
+    json_data = json.dumps({
+        'status': 'Query started'
+    })
+    funcUrl = "http://localhost:7071/api/send_status"
+    response = requests.post(url=funcUrl, data=json_data)
+    time.sleep(waitTime1)
 
-@app.function_name(name="modify_message")
-@app.route(route="modify_message", methods=["POST"])
-@app.generic_output_binding(
-    arg_name="signalRMessages",
-    type="signalR",
-    hub_name="bdotstsignal",
-    connection_string_setting="AzureSignalRConnectionString"
-)
-def modify_message(req: func.HttpRequest, signalRMessages: func.Out[str]) -> func.HttpResponse:
-    logging.info('modify_message function received a request.')
+    json_data = json.dumps({
+        'status': 'Getting sources'
+    })
+    funcUrl = "http://localhost:7071/api/send_status"
+    response = requests.post(url=funcUrl, data=json_data)
+    time.sleep(waitTime2)
 
-    try:
-        # Parse the JSON body
-        req_body = req.get_json()
-        message = req_body.get('message')
-
-        if message is None:
-            return func.HttpResponse(
-                "Please pass a message in the request body",
-                status_code=400
-            )
-
-        # Reverse the message
-        reversed_message = message[::-1]
-
-        # Create a SignalR message
-        signalr_message = {
-            'target': 'newMessage',
-            'arguments': [reversed_message]
-        }
-
-        # Set the SignalR message output
-        signalRMessages.set(json.dumps(signalr_message))
-
-        return func.HttpResponse(f"Message received and broadcasted: {reversed_message}")
-
-    except ValueError:
-        return func.HttpResponse(
-            "Invalid JSON body",
-            status_code=400
-        )
-
-    except Exception as e:
-        logging.error(f"Error processing the request: {str(e)}")
-        return func.HttpResponse(
-            "An error occurred processing the request",
-            status_code=500
-        )
+    signalRMessages.set(json.dumps({
+        'target': 'newMessage',
+        'arguments': ['this is a newer message']
+    }))
+    return "ok"
